eee4f67f12045bc1865eed02a1eb0e05e1e9598c
Vadym Yatsyuk <vasp3d@gmail.com>
2016-04-28 12:33:28 +0200

    init

---
+++ PomodoroApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PomodoroApp
+//
+//  Created by Vadim Dez on 28/04/16.
+//  Copyright © 2016 Vadim Dez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PomodoroApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PomodoroApp
+//
+//  Created by Vadim Dez on 28/04/16.
+//  Copyright © 2016 Vadim Dez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
