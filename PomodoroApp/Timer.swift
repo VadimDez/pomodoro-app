9e3bd13944f47d31d73813a1545684db1e19319c
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-04-28 22:02:35 +0200

    timer

---
+++ PomodoroApp/Timer.swift
@@ -0,0 +1,63 @@
+//
+//  Timer.swift
+//  PomodoroApp
+//
+//  Created by Vadim Dez on 28/04/16.
+//  Copyright © 2016 Vadim Dez. All rights reserved.
+//
+
+import Foundation
+
+class Timer : NSObject {
+    
+    private var minutes: Int!
+    private var seconds: Int!
+    private var callback: (minutes: Int, seconds: Int) -> Void
+    private var timer: NSTimer!
+    
+    init(minutes: Int, seconds: Int, callback: (minutes: Int, seconds: Int) -> Void) {
+        self.minutes = minutes
+        self.seconds = seconds
+        self.callback = callback
+    }
+    
+    
+    func start() {
+        self.setupTimer()
+    }
+    
+    func stop() {
+        self.timer.invalidate()
+    }
+    
+    private func setupTimer() {
+        if (self.timer !== nil) {
+            self.timer.invalidate()
+        }
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(cycle), userInfo: nil, repeats: true)
+    }
+    
+    func cycle(sender: AnyObject) {
+        self.seconds = self.seconds - 1
+        
+        if (self.seconds < 0) {
+            self.seconds = 59
+            self.minutes = self.minutes - 1
+            
+            if (self.minutes < 0) {
+                self.stop()
+            }
+        }
+        
+        self.callback(minutes: self.minutes, seconds: self.seconds)
+    }
+    
+    func getMinutes() -> Int {
+        return self.minutes
+    }
+    
+    func getSeconds() -> Int {
+        return self.seconds
+    }
+}
--- PomodoroApp/ViewController.swift
+++ PomodoroApp/ViewController.swift
@@ -13,11 +13,23 @@ class ViewController: NSViewController {
     @IBOutlet weak var minutesLabel: NSTextField!
     @IBOutlet weak var secondsLabel: NSTextField!
     
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.timer = Timer(minutes: 25, seconds: 0, callback: self.callback)
+    }
+    
+    func callback (minutes: Int, seconds: Int) {
+        self.minutesLabel.stringValue = self.doubleNumber(minutes)
+        self.secondsLabel.stringValue = self.doubleNumber(seconds)
+    }
+    
+    func doubleNumber(number: Int) -> String {
+        return String("0\(number)".characters.suffix(2))
     }
 
     override var representedObject: AnyObject? {
@@ -27,12 +39,11 @@ class ViewController: NSViewController {
     }
 
     @IBAction func onStop(sender: AnyObject) {
-        print("stop")
+        self.timer.stop()
     }
 
     @IBAction func onStart(sender: AnyObject) {
-        print("start")
-        self.minutesLabel.stringValue = "24"
+        self.timer.start()
     }
 }
 
