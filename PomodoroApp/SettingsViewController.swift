4fd1402d1d69e0daad954ecf0eb933c7f66caab9
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-05-05 15:34:31 +0200

    minor code changes, added app icons

--- PomodoroApp/SettingsViewController.swift
+++ PomodoroApp/SettingsViewController.swift
@@ -14,7 +14,8 @@ class SettingsViewController: NSViewController {
     @IBOutlet weak var restLengthField: NSTextField!
     @IBOutlet weak var soundsCheckbox: NSButton!
     
-    var defaults: NSUserDefaults!
+    private var defaults: NSUserDefaults!
+    private var hasErrors: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,27 +29,15 @@ class SettingsViewController: NSViewController {
     @IBAction func dismissSettings(sender: AnyObject) {
         let sessionLength = Int(self.sessionLengthField.stringValue)
         let restLength = Int(self.restLengthField.stringValue)
-        var error = false
         
-        if (sessionLength < 1) {
-            error = true
-            self.sessionLengthField.backgroundColor = NSColor(red: 1, green: 0, blue: 0, alpha: 0.3)
-        } else {
-            self.sessionLengthField.backgroundColor = NSColor.whiteColor()
-        }
-        
-        if (restLength < 1) {
-            error = true
-            self.restLengthField.backgroundColor = NSColor(red: 1, green: 0, blue: 0, alpha: 0.3)
-        } else {
-            self.restLengthField.backgroundColor = NSColor.whiteColor()
-        }
+        self.hasErrors = false
+        self.validate(sessionLength!, textField: self.sessionLengthField)
+        self.validate(restLength!, textField: self.restLengthField)
         
         self.sessionLengthField.window?.makeFirstResponder(nil)
-        
         self.restLengthField.window?.makeFirstResponder(nil)
         
-        if (error) {
+        if (self.hasErrors) {
             NSBeep()
             return
         }
@@ -60,7 +49,19 @@ class SettingsViewController: NSViewController {
         self.dismissViewController(self)
     }
     
-    func setTimes() {
+    /**
+     * Validate text field
+     */
+    private func validate(value: Int, textField: NSTextField) {
+        if (value < 1) {
+            self.hasErrors = true
+            textField.backgroundColor = NSColor(red: 1, green: 0, blue: 0, alpha: 0.3)
+        } else {
+            textField.backgroundColor = NSColor.whiteColor()
+        }
+    }
+    
+    private func setTimes() {
         if let sessionLength = self.defaults.stringForKey(DataKeys.sessionLength) {
             self.sessionLengthField.stringValue = sessionLength
         } else {
@@ -74,7 +75,7 @@ class SettingsViewController: NSViewController {
         }
     }
     
-    func setSounds() {
+    private func setSounds() {
         if let soundsEnabled = self.defaults.valueForKey(DataKeys.sounds) {
             self.soundsCheckbox.state = Int(soundsEnabled as! NSNumber)
         } else {
